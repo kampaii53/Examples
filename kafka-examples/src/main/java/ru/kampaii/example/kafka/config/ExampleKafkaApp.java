de486eb7943f0d7d25edac10d057e76b6f6599b6
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-05-21 12:36:12 +0500

    Added simple Kafka example

--- kafka-examples/src/main/java/ru/kampaii/example/kafka/config/ExampleKafkaApp.java
+++ kafka-examples/src/main/java/ru/kampaii/example/kafka/config/ExampleKafkaApp.java
@@ -2,11 +2,20 @@ package ru.kampaii.example.kafka.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
 
+@EnableKafka
 @SpringBootApplication(scanBasePackages = "ru.kampaii.example.kafka")
 public class ExampleKafkaApp {
 
     public static void main(String[] args) {
         SpringApplication.run(ExampleKafkaApp.class);
     }
+
+    @KafkaListener(topics="msg")
+    public void msgListener(String msg){
+        System.out.println(msg);
+    }
+
 }
